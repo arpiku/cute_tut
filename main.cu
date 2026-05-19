18733ae3f8863874873712eadc4d1d0f2fe96516
arpiku <akphysics03@gmail.com>
2026-05-19 14:15:20 +0530

    Add seperate file printer for tex files

--- main.cu
+++ main.cu
@@ -1,8 +1,67 @@
+#include "cute/stride.hpp"
 #include <cute/util/print_latex.hpp>
 
-int main() {
-  // auto layout = cute::make_layout(cute::Shape<cute::_2, cute::_3>{});
-  auto layout = cute::make_layout(cute::Shape< cute::Shape <cute::_2, cute::_3> , cute::_4>{});
+#include <cstdio>
+#include <fcntl.h>
+#include <string>
+#include <unistd.h>
+
+using namespace cute;
+
+namespace {
+
+template <class Layout>
+bool print_latex_to_file(std::string const& filename, Layout const& layout) {
+  std::fflush(stdout);
+
+  int saved_stdout = dup(STDOUT_FILENO);
+  if (saved_stdout < 0) {
+    std::perror("dup");
+    return false;
+  }
+
+  int fd = open(filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (fd < 0) {
+    std::perror(filename.c_str());
+    close(saved_stdout);
+    return false;
+  }
+
+  if (dup2(fd, STDOUT_FILENO) < 0) {
+    std::perror("dup2");
+    close(fd);
+    close(saved_stdout);
+    return false;
+  }
+
+  close(fd);
+
   cute::print_latex(layout);
+  std::fflush(stdout);
+
+  if (dup2(saved_stdout, STDOUT_FILENO) < 0) {
+    std::perror("dup2-restore");
+    close(saved_stdout);
+    return false;
+  }
+
+  close(saved_stdout);
+  return true;
+}
+
+} // namespace
+
+int main() {
+  auto row_major_30x10 = make_layout(make_tuple(Int<30>{}, Int<10>{}), LayoutRight());
+  auto col_major_30x10 = make_layout(make_tuple(Int<30>{}, Int<10>{}), LayoutLeft());
+
+  if (!print_latex_to_file("row_major_30x10.tex", row_major_30x10)) {
+    return 1;
+  }
+
+  if (!print_latex_to_file("col_major_30x10.tex", col_major_30x10)) {
+    return 1;
+  }
+
   return 0;
 }
